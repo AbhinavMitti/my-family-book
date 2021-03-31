3de8646b1cecaa796b7f5a164af15c469b44b7e4
AbhinavMitti <77564368+AbhinavMitti@users.noreply.github.com>
2021-03-31 12:16:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function changetext() {
+    var x = document.getElementById("names_change");
+    if (x.innerHTML === "i have two members in my family") {
+        x.innerHTML = "my father:Satish.Mitti";
+    } else {
+        x.innerHTML = "my mother:usha.mitti";
+    }
+}
